548b311a898d7c9f77a707e4774e74f2f294f700
Admin <admin@admins-mbp.hub>
2019-10-23 20:35:47 +1300

    Initial Commit

---
+++ just-exploring-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  just-exploring-3
+//
+//  Created by Admin on 23/10/2019.
+//  Copyright © 2019 mattweir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ just-exploring-3Tests/just_exploring_3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  just_exploring_3Tests.swift
+//  just-exploring-3Tests
+//
+//  Created by Admin on 23/10/2019.
+//  Copyright © 2019 mattweir. All rights reserved.
+//
+
+import XCTest
+@testable import just_exploring_3
+
+class just_exploring_3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
